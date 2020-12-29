62e3ccfd697572930f328f38959b6f4d4adb59b2
Karol Stryczek <karol531531@gmail.com>
2020-12-29 23:15:27 +0100

    Fixed bug in data parsing and saving.

--- acquisition_app/ParserUtil.py
+++ acquisition_app/ParserUtil.py
@@ -11,15 +11,16 @@ def parse_new_cases(new_cases: List[Status]) -> None:
     first_tweet = new_cases[0]
     tweet_text = first_tweet.full_text + ' ' + new_cases[1].full_text
     new_cases_data = parse_tweet_text(tweet_text)
-    new_cases_with_date, date = populate_new_cases_with_date(new_cases_data, first_tweet.created_at)
-    dao.save_new_cases(new_cases_with_date)
+    if sum(new_cases_data.cases.values) > 0:
+        new_cases_with_date, date = populate_new_cases_with_date(new_cases_data, first_tweet.created_at)
+        dao.save_new_cases(new_cases_with_date)
 
 
 def parse_tweet_text(tweet_text: str) -> pd.DataFrame:
     voivodeships = dao.get_voivodeships()
     new_cases_table = list()
     for voivodeship_genitive_name in voivodeships['name_genitive'].values:
-        regex_pattern = re.compile(rf' {voivodeship_genitive_name} \(\d+')
+        regex_pattern = re.compile(rf'[\n|\s]{voivodeship_genitive_name} \(\d+')
         matched_object = regex_pattern.search(tweet_text)
         name = voivodeships[voivodeships['name_genitive'] == voivodeship_genitive_name]['name'].values[0]
         if matched_object is not None:
